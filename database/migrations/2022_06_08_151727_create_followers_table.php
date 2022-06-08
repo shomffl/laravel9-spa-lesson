9bc4480c5199bbdbbd701a54ac9b153ea8a9e189
sho watanabe <showatanabe0226@gmail.com>
2022-06-09 00:21:07 +0900

    [add]フォロー機能用のマイグレーションファイルを作成

---
+++ database/migrations/2022_06_08_151727_create_followers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->foreignId("following_id")->constrained("users");
+            $table->foreignId("followed_id")->constrained("users");
+            $table->primary(["following_id","followed_id"]);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('followers');
+    }
+};
