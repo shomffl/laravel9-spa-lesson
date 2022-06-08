762a8f02a707d9cbe585d7ffe83163b16248197e
sho watanabe <showatanabe0226@gmail.com>
2022-06-08 21:49:33 +0900

    [add]postsデータ用のseederとfactoryを作成

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Post;
+
+class PostFactory extends Factory
+{
+    protected $model = Post::class;
+
+    public function definition()
+    {
+        return [
+            "title" => $this->faker->word,
+            "description" => $this->faker->word,
+            "user_id" => 1,
+            "category_id" => 1,
+            "created_at" => date("Y-m-d H:i:s"),
+            "updated_at" => date("Y-m-d H:i:s")
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersTableSeeder::class,
             CategoriesTableSeeder::class,
+            PostsTableSeeder::class
         ]);
     }
 }
